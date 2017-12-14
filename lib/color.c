90537197e1e740c9a08353514a0dc88124c9c355
Andreas Dixius <Andreas.Dixius@tu-dresden.de>
2017-12-14 10:15:40 +0100

    colors update

--- lib/color.c
+++ lib/color.c
@@ -9,6 +9,8 @@
 
 #include "color.h"
 
+#define CHAR_COLOR_SIZE 16
+
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_GREEN   "\x1b[32m"
 #define ANSI_COLOR_YELLOW  "\x1b[33m"
--- lib/color.h
+++ lib/color.h
@@ -16,16 +16,14 @@
 extern "C" {
 #endif
 
-
-#define CHAR_COLOR_SIZE 10
-extern gchar color_red     [CHAR_COLOR_SIZE];
-extern gchar color_green   [CHAR_COLOR_SIZE];
-extern gchar color_yellow  [CHAR_COLOR_SIZE];
-extern gchar color_blue    [CHAR_COLOR_SIZE];
-extern gchar color_magenta [CHAR_COLOR_SIZE];
-extern gchar color_cyan    [CHAR_COLOR_SIZE];
-extern gchar color_reset   [CHAR_COLOR_SIZE];
-extern gchar color_bold    [CHAR_COLOR_SIZE];
+extern gchar color_red     [];
+extern gchar color_green   [];
+extern gchar color_yellow  [];
+extern gchar color_blue    [];
+extern gchar color_magenta [];
+extern gchar color_cyan    [];
+extern gchar color_reset   [];
+extern gchar color_bold    [];
 
 void colors_on ();
 void colors_off ();
