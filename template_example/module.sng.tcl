f8e2b588524157e6d791e702b95f4eb91fd4732d
Andreas <andreas.dixius@posteo.de>
2017-12-25 14:37:52 +0100

    sng-parse: module/instance creation

--- template_example/module.sng.tcl
+++ template_example/module.sng.tcl
@@ -1,5 +1,7 @@
 #!/usr/bin/tclsh
 
+load ../lib/icglue.so
+
 proc sng_split_instances {instances} {
     set result {}
     foreach i_e $instances {
@@ -44,6 +46,8 @@ proc parse_sng_line {number line} {
             return [list $number "comment" $mcomment]
         } elseif {[regexp {^[\s]*//(.*)$} $line mline mcomment]} {
             return [list $number "comment" $mcomment]
+        } elseif {[regexp {^[\s]*$} $line mline]} {
+            return [list $number "ignore"]
         } elseif {[regexp {^[\s]*M:[\s]*([[:alnum:]_]+)[\s]*(\((.*)\))?[\s]*:=([^#/]+)?$} $line mline mmodule m_args mmoduleargs minstances]} {
             set args [split [string map {" " {} "\t" {}} $mmoduleargs] ","]
             set insts [split $minstances " \t"]
@@ -58,13 +62,13 @@ proc parse_sng_line {number line} {
             set targets [lsearch -inline -all -not $targets {}]
             return [list $number "signal" $sig $assign $arrow [sng_split_instances $start] [sng_split_instances $targets]]
         } elseif {[regexp {^[\s]*G:[\s]*([[:alnum:]_]+)[\s]*(\((.*)\))?[\s]*:=[\s]*([^ \t]+)[\s]*(<-|<->|->)(.*)} $line mline mpar m_assign massign mstart marrow mtargets]} {
-            set sig $msig
+            set par $mpar
             set assign $massign
             set arrow $marrow
             set start $mstart
             set targets [split $mtargets " \t"]
             set targets [lsearch -inline -all -not $targets {}]
-            return [list $number "signal" $sig $assign $arrow [sng_split_instances $start] [sng_split_instances $targets]]
+            return [list $number "parameter" $par $assign $arrow [sng_split_instances $start] [sng_split_instances $targets]]
         } elseif {[regexp {^[\s]*C:[\s]*([[:alnum:]_]+)[\s]*:=[\s]*begin} $line mline mmod]} {
             set parse_sng_line_codemod $mmod
             set parse_sng_line_codelist [list]
@@ -84,6 +88,70 @@ proc parse_sng_line {number line} {
     }
 }
 
+proc evaluate_sng_lines {parsed_lines} {
+    # modules
+    foreach i_mod [lsearch -all -inline -index 1 $parsed_lines "module"] {
+        set linenumber [lindex $i_mod 0]
+        set modname    [lindex $i_mod 2]
+        set modargs    [lindex $i_mod 3]
+
+        set lang "verilog"
+        set mode "rtl"
+
+        set ilm      "false"
+        set resource "false"
+        foreach i_arg $modargs {
+            switch $i_arg {
+                "verilog"       {set lang $i_arg}
+                "vhdl"          {set lang $i_arg}
+                "systemverilog" {set lang $i_arg}
+                "rtl"           {set mode $i_arg}
+                "tb"            {set mode $i_arg}
+                "behavioral"    {set mode $i_arg}
+                "ilm"           {set ilm      "true"}
+                "resource"      {set resource "true"}
+                default         {error "line ${linenumber}: could not parse module args"}
+            }
+        }
+        if {$resource} {
+            set modid [create_module -resource -name $modname]
+        } elseif {$ilm} {
+            set modid [create_module -ilm -name $modname]
+        } else {
+            set modid [create_module -name $modname]
+        }
+        set_attribute -object $modid -attribute "language" -value $lang
+        set_attribute -object $modid -attribute "mode"     -value $mode
+    }
+
+    # instances
+    foreach i_mod [lsearch -all -inline -index 1 $parsed_lines "module"] {
+        set linenumber [lindex $i_mod 0]
+        set parentmod  [lindex $i_mod 2]
+        set insts      [lindex $i_mod 4]
+        foreach i_inst $insts {
+            set mod       [lindex $i_inst 0]
+            set inst_name [lindex $i_inst 1]
+
+            if {[catch {\
+                    create_instance \
+                        -name $inst_name \
+                        -of-module [get_modules -name $mod] \
+                        -parent-module [get_modules -name $parentmod] \
+                }]} {
+                error "line ${linenumber}: could not create instance"
+            }
+        }
+    }
+
+    # parameter
+    foreach i_param [lsearch -all -inline -index 1 $parsed_lines "parameter"] {
+        # TODO
+    }
+
+    # TODO: code, signals
+}
+
 proc parse_sng_file {filename} {
     set f [open $filename "r"]
     set lines [split [read $f] "\n"]
@@ -91,7 +159,7 @@ proc parse_sng_file {filename} {
 
     set i 1
     set current ""
-    set parsed {}
+    set parsed [list]
 
     foreach i_l $lines {
         if {[string index $i_l end] == "\\"} {
@@ -103,4 +171,6 @@ proc parse_sng_file {filename} {
         }
         incr i
     }
+
+    evaluate_sng_lines $parsed
 }
