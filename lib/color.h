816e72ad253644da5c76b98780a15670c366c8df
Andreas Dixius <Andreas.Dixius@tu-dresden.de>
2018-04-30 12:33:48 +0200

    added license header/printout

--- lib/color.h
+++ lib/color.h
@@ -1,11 +1,21 @@
-/*       _\|/_
-         (o o)
- +----oOO-{_}-OOo-------------+
- |          _      ^..^       |
- |    _   _(_     ( oo )  )~  |
- |  _) /)(// (/     ,,  ,,    |
- |                2017-12-13  | 
- +---------------------------*/
+/*
+ *  ICGlue is a Tcl-Library for scripted HDL generation
+ *  Copyright (C) 2017-2018  Andreas Dixius, Felix Neumärker
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
 
 #ifndef __COLOR_H__
 #define __COLOR_H__
--- lib/icglue.c
+++ lib/icglue.c
@@ -1,3 +1,22 @@
+/*
+ *  ICGlue is a Tcl-Library for scripted HDL generation
+ *  Copyright (C) 2017-2018  Andreas Dixius, Felix Neumärker
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 #include <tcl.h>
 #include <stdio.h>
 
@@ -10,6 +29,9 @@ int Icglue_Init (Tcl_Interp *interp) {
 
     colors_on ();
     log_info ("PLoad", "ICGlue v0.0.1");
+    log_info ("PLoad", "a Tcl-Library for scripted HDL generation");
+    log_info ("PLoad", "Copyright (C) Andreas Dixius, Felix Neumärker");
+    log_info ("PLoad", "Use and redistribute under the terms of the GNU General Public License version 3");
 
     ig_add_tcl_commands (interp);
 
