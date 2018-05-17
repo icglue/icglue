b36c74cf7933950fe2b7b1de4d1ed9b9f7e70025
Andreas Dixius <Andreas.Dixius@tu-dresden.de>
2018-05-17 09:45:01 +0200

    ig_lib commented

--- lib/ig_lib.h
+++ lib/ig_lib.h
@@ -70,27 +70,128 @@ struct ig_lib_connection_info {
     struct ig_object          *obj;         /**< @brief Object of signal/parameter hierarchy (instance or module object). */
     const char                *parent_name; /**< @brief Signal/parameter name at upper hierarchy level or @c NULL. */
     const char                *local_name;  /**< @brief Signal/parameter name at current hierarchy level or @c NULL. */
-    bool                       is_explicit; /**< @brief Signal/parameter name was explicitly set at this hierarchy point. */
+    bool                       is_explicit; /**< @brief Hierarchy element is explicitly set. */
     bool                       force_name;  /**< @brief Signal/parameter name must be kept verbatim. */
     enum ig_lib_connection_dir dir;         /**< @brief Signal direction in hierarchy.*/
 };
 
+/**
+ * @brief Create and initialize new icglue database struct.
+ * @return The newly created struct.
+ */
 struct ig_lib_db *ig_lib_db_new  ();
-void              ig_lib_db_free (struct ig_lib_db *db);
 
-struct ig_module   *ig_lib_add_module      (struct ig_lib_db *db, const char *name, bool ilm, bool resource);
+/**
+ * @brief Free icglue database struct.
+ * @param db Database to free.
+ */
+void ig_lib_db_free (struct ig_lib_db *db);
+
+/**
+ * @brief Add a new module to the database.
+ * @param db Database to modify.
+ * @param name Name of the module.
+ * @param ilm ILM property of module to add.
+ * @param resource Resource property of module to add.
+ * @return The newly created module data or @c NULL.
+ */
+struct ig_module *ig_lib_add_module      (struct ig_lib_db *db, const char *name, bool ilm, bool resource);
+
+/**
+ * @brief Add a new module instance to the database.
+ * @param db Database to modify.
+ * @param name Name of the instance.
+ * @param type Module to instanciate.
+ * @param parent Module in which instance is to be added.
+ * @return The newly created instance data or @c NULL.
+ */
 struct ig_instance *ig_lib_add_instance    (struct ig_lib_db *db, const char *name, struct ig_module *type, struct ig_module *parent);
-struct ig_code     *ig_lib_add_codesection (struct ig_lib_db *db, const char *name, const char *code, struct ig_module *parent);
 
+/**
+ * @brief Add a new codesection to the database.
+ * @param db Database to modify.
+ * @param name Name of new codesection or @c NULL.
+ * @param code Code to add.
+ * @param parent Module in which codesection is to be added.
+ * @return The newly created codesection data or @c NULL.
+ */
+struct ig_code *ig_lib_add_codesection (struct ig_lib_db *db, const char *name, const char *code, struct ig_module *parent);
+
+/**
+ * @brief Add a new regfile to the database.
+ * @param db Database to modify.
+ * @param name Name of new regfile.
+ * @param parent Module in which regfile is to be added.
+ * @return The newly created regfile data or @c NULL.
+ */
 struct ig_rf_regfile *ig_lib_add_regfile       (struct ig_lib_db *db, const char *name, struct ig_module *parent);
-struct ig_rf_entry   *ig_lib_add_regfile_entry (struct ig_lib_db *db, const char *name, struct ig_rf_regfile *parent);
-struct ig_rf_reg     *ig_lib_add_regfile_reg   (struct ig_lib_db *db, const char *name, struct ig_rf_entry *parent);
 
+/**
+ * @brief Add a new regfile-entry to the database.
+ * @param db Database to modify.
+ * @param name Name of new regfile-entry.
+ * @param parent Regfile in which regfile-entry is to be added.
+ * @return The newly created regfile-entry data or @c NULL.
+ */
+struct ig_rf_entry *ig_lib_add_regfile_entry (struct ig_lib_db *db, const char *name, struct ig_rf_regfile *parent);
+
+/**
+ * @brief Add a new register to the database.
+ * @param db Database to modify.
+ * @param name Name of new register.
+ * @param parent Regfile-entry in which register is to be added.
+ * @return The newly created register data or @c NULL.
+ */
+struct ig_rf_reg *ig_lib_add_regfile_reg   (struct ig_lib_db *db, const char *name, struct ig_rf_entry *parent);
+
+/**
+ * @brief Create new connection info data.
+ * @param str_chunks String container to use for newly created strings.
+ * @param obj Object represented by this hierarchy element.
+ * @param local_name Local name of signal/paramater at this hierarchy element or @c NULL.
+ * @param dir Signal direction at this element.
+ * @return The newly created connection info data or @c NULL.
+ */
 struct ig_lib_connection_info *ig_lib_connection_info_new  (GStringChunk *str_chunks, struct ig_object *obj, const char *local_name, enum ig_lib_connection_dir dir);
-struct ig_lib_connection_info *ig_lib_connection_info_copy (GStringChunk *str_chunks, struct ig_lib_connection_info *original);
-void                           ig_lib_connection_info_free (struct ig_lib_connection_info *cinfo);
 
+/**
+ * @brief Create a copy of existing connection info data.
+ * @param str_chunks String container to use for copies of strings.
+ * @param original connection info to copy.
+ * @return A copy of @c original or NULL.
+ */
+struct ig_lib_connection_info *ig_lib_connection_info_copy (GStringChunk *str_chunks, struct ig_lib_connection_info *original);
+
+/**
+ * @brief Free connection info data.
+ * @param cinfo connection info data to free.
+ */
+void ig_lib_connection_info_free (struct ig_lib_connection_info *cinfo);
+
+/**
+ * @brief Create hierarchical signal.
+ * @param db Database to use.
+ * @param signame Signal name.
+ * @param source Signal source or @c NULL. Will be freed after usage.
+ * @param targets List of signal endpoints. List data: <tt> (struct @ref ig_lib_connection_info *) </tt>. Will be freed after usage.
+ * @param[out] gen_objs List of newly created hierarchy pin/port/declaration elements of signal. List data: <tt> (struct ig_object *) </tt>.
+ * @return true on success.
+ *
+ * @c source and @c targets will be freed on return, @c *gen_objs must be freed by caller.
+ */
 bool ig_lib_connection (struct ig_lib_db *db, const char *signame, struct ig_lib_connection_info *source, GList *targets, GList **gen_objs);
+
+/**
+ * @brief Create hierarchical parameter.
+ * @param db Database to use.
+ * @param parname Parameter name.
+ * @param defvalue Parameter default value.
+ * @param targets List of parameter endpoints. List data: <tt> (struct @ref ig_lib_connection_info *) </tt>. Will be freed after usage.
+ * @param[out] gen_objs List of newly created hierarchy parameter/adjustment elements of parameter. List data: <tt> (struct ig_object *) </tt>.
+ * @return true on success.
+ *
+ * @c targets will be freed on return, @c *gen_objs must be freed by caller.
+ */
 bool ig_lib_parameter  (struct ig_lib_db *db, const char *parname, const char *defvalue, GList *targets, GList **gen_objs);
 
 #ifdef __cplusplus
