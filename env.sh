715c87fb036e0e54c6d307bfd6e51fe672788ed9
snafu <snafu@euclid>
2018-05-18 18:14:06 +0200

    env.sh: add MANPATH

--- env.sh
+++ env.sh
@@ -3,6 +3,8 @@ src=${BASH_SOURCE:-$_}
 pkgsrcpath=$(dirname $(readlink -e "${src}"))
 
 export PATH="${PATH}:${pkgsrcpath}/bin"
+MANPATH=${MANPATH:-/usr/share/man}
+export MANPATH="${MANPATH}:${pkgsrcpath}/doc/ICGlue/man"
 
 unset src
 unset pkgsrcpath
