9f9a7baae3682457131b265a6686436a77e5fa18
snafu <snafu@euclid>
2018-08-23 16:39:01 +0200

    env.sh: update man path

--- env.sh
+++ env.sh
@@ -4,12 +4,13 @@ pkgsrcpath=$(dirname $(readlink -e "${src}"))
 
 export PATH="${PATH}:${pkgsrcpath}/bin"
 MANPATH=${MANPATH:-/usr/share/man}
-export MANPATH="${MANPATH}:${pkgsrcpath}/man:${pkgsrcpath}/doc/ICGlue/man"
+export MANPATH="${MANPATH}:${pkgsrcpath}/share/man:${pkgsrcpath}/doc/ICGlue/man"
 
-# remove logo header
-eval "icg() { command icglue \$@ 2>&1 | tail -n+$(($(wc -l ${pkgsrcpath}/logo/logo.txt| cut -f1 -d" ") + 2)) }"
 
 if [[ -n ${ZSH_VERSION} ]] ; then
+    # remove logo header
+    eval "icg() { command icglue \$@ 2>&1 | tail -n+$(($(wc -l ${pkgsrcpath}/logo/logo.txt| cut -f1 -d" ") + 2)) }"
+
     compdef _gnu_generic icg
 fi
 
