477478f2c720ae747b030b133a55494bfc8e63c2
Andreas Dixius <Andreas.Dixius@tu-dresden.de>
2018-09-11 12:04:40 +0200

    doc update

--- tcllib/icglue_construct.tcl
+++ tcllib/icglue_construct.tcl
@@ -678,10 +678,11 @@ namespace eval ig {
     #      <tr><td><b> OPTION </b></td><td><br></td></tr>
     #      <tr><td><i> &ensp; &ensp; -a(dapt)                 </i></td><td>  adapt signal names                                                                        <br></td></tr>
     #      <tr><td><i> &ensp; &ensp; -noa(dapt)               </i></td><td>  do not adapt signal names                                                                 <br></td></tr>
+    #      <tr><td><i> &ensp; &ensp; -a(dapt-)s(elective(ly)) </i></td><td>  selectively adapt signal names followed by "!"                                            <br></td></tr>
     #      <tr><td><i> &ensp; &ensp; -s(ubst)                 </i></td><td>  perform Tcl-variable substition of CODE argument, do not forget to escape, esp \[ and \]  <br></td></tr>
     #      <tr><td><i> &ensp; &ensp; -nos(ubst)               </i></td><td>  do not perform Tcl-variable substition of CODE argument                                   <br></td></tr>
     #      <tr><td><i> &ensp; &ensp; -v(erbatim)              </i></td><td>  alias for -noadapt and -nosubst                                                           <br></td></tr>
-    #      <tr><td><i> &ensp; &ensp; -e((val)ulate)           </i></td><td>  perform Tcl substition of CODE argument, do not forget to escape                          <br></td></tr>
+    #      <tr><td><i> &ensp; &ensp; -e(val(ulate))           </i></td><td>  perform Tcl substition of CODE argument, do not forget to escape                          <br></td></tr>
     #      <tr><td><i> &ensp; &ensp; -noi(dentfix)            </i></td><td>  do not fix the indent of the codeblock                                                    <br></td></tr>
     #
     #    </table>
