3ed135df6e6598c89e505f24151801899cd53641
Andreas Dixius <Andreas.Dixius@tu-dresden.de>
2018-09-18 13:40:58 +0200

    Release version 1.0

--- tcllib/icglue_templates.tcl
+++ tcllib/icglue_templates.tcl
@@ -17,7 +17,7 @@
 #   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-package provide ICGlue 1.0a1
+package provide ICGlue 1.0
 
 ## @brief Template related functionality
 namespace eval ig::templates {
