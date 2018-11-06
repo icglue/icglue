b98d50749043863e0990df96236488196bbd859c
Heiner Bauer <bauer@racyics.de>
2018-11-06 15:47:35 +0100

    emacs icglue-mode: implement Verilog syntax highlighting

--- emacs/icglue-mode.el
+++ emacs/icglue-mode.el
@@ -62,6 +62,82 @@
   table)
   "Syntax table used in ICGlue mode buffers.")
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; TODO: fix '#' comment highlighting from TCL applied *after* verilog highlighting
+;; TODO: figure out how to avoid 'modified' buffer state after highlighting verilog syntax
+;; Graciously inspired by http://emacs.stackexchange.com/a/5408/227
+;; Run Verilog mode for syntax highlighting in temporary buffer
+(defun icglue-fontify-verilog (text)
+  "Add `font-lock-face' properties to Verilog text. Return a fontified copy of TEXT."
+  (with-temp-buffer
+    (erase-buffer)
+    (insert text)
+    ;; Run Verilog mode without any hooks
+    (delay-mode-hooks
+      (verilog-mode)
+      (font-lock-mode))
+    (font-lock-ensure)
+    ;; Convert `face' to `font-lock-face' to play nicely with font lock
+    (goto-char (point-min))
+    (while (not (eobp))
+      (let ((pos (point)))
+        (goto-char (next-single-property-change pos 'face nil (point-max)))
+        (put-text-property pos (point) 'font-lock-face
+                           (get-text-property pos 'face))))
+    (buffer-string)))
+
+(defun icglue-fontify-verilog-section (section)
+  "Replace text of SECTION with text fontified by verilog-mode."
+  (interactive)
+  (when section
+    (setq start (nth 0 section))
+    (setq end   (nth 1 section))
+    (setq text (buffer-substring-no-properties start end))
+    (setq fontified-text (icglue-fontify-verilog text))
+    (delete-region start end)
+    (goto-char start)
+    (insert fontified-text)))
+
+(defun icglue-next-code-section ()
+  "Return the start / end pair of the next ICGlue code section after point."
+  (interactive)
+  (when (re-search-forward "^ ?C.+{" nil t)
+    (backward-char 1)
+    (forward-sexp) ;; move point to the closing '}' of the code section
+    (let ((start (match-end 0))
+          (end (point)))
+      (list start end))))
+
+(defun icglue-print-next-code-section ()
+  (interactive)
+  (let ((pair (icglue-next-code-section)))
+    (if pair
+        (message (format "start:%d end:%d" (nth 0 pair) (nth 1 pair))))))
+
+(defun icglue-code-sections ()
+  "Return a list with start / end pairs for all ICGlue code sections in the buffer."
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (let ((current-section (icglue-next-code-section))
+          (section-list ()))
+      (while current-section
+        (setq section-list (append (list current-section) section-list))
+        (setq current-section (icglue-next-code-section)))
+      section-list)))
+
+(defun icglue-fontify-code-sections ()
+  "Fontify Verilog code sections in the current buffer."
+  (interactive)
+  (save-excursion
+  (let ((code-sections (icglue-code-sections)))
+    (while code-sections
+      (setq current-section (pop code-sections))
+      (icglue-fontify-verilog-section current-section)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (require 'compile)
 
 (defvar icglue-mode-map
