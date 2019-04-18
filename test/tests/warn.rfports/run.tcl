586c548cd5929d07afb83c6e22fa9a9bc035fa6c
Andreas Dixius <andreas.dixius@icglue.org>
2019-04-18 09:15:18 +0200

    tests: renamed

--- test/tests/warn.rfports/run.tcl
+++ test/tests/warn.rfports/run.tcl
@@ -6,6 +6,7 @@ run icprep project
 eval_run_output {glob {I,Gen*} 4}
 
 run_nocheck icglue units/mod/source/gen/mod.icglue -o "vlog-v"
+# check for missing regfile ports warnings
 eval_run_output {
     glob {W,RFTP*mod_rf: Missing port*} 13
 }
