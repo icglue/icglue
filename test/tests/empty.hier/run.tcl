e72d9ca3c26325021933a151a338960a14082c32
Andreas Dixius <andreas.dixius@icglue.org>
2019-04-18 09:15:38 +0200

    tests: added test for empty mod/rf

---
+++ test/tests/empty.hier/run.tcl
@@ -0,0 +1,14 @@
+# test files
+deploy mod.icglue           units/mod/source/gen/
+
+# setup
+run icprep project
+
+# generate everything
+run icglue units/mod/source/gen/mod.icglue
+
+# check for 0 warnings/errors
+eval_run_output {
+    glob {W,*:*} 0
+    glob {E,*:*} 0
+}
