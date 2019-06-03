afd2792e23fe47393c9694bc99d4ae841fc1698b
Heiner Bauer <bauer@racyics.de>
2019-06-03 15:14:59 +0200

    avoid confusion with proc and provide equivalent tree

--- examples/module.construct.simple.tcl
+++ examples/module.construct.simple.tcl
@@ -1,24 +1,52 @@
-# modules
-M -rtl -v -u proc proc_mgmt
-M -rtl -v -u proc proc_core
-M -rtl -v -u proc proc_top    -i {proc_core proc_mgmt}
-M -tb  -v -u proc tb_proc_top -i {proc_top}
+# without -tree in bottom to top order
+M -rtl -v -u bungo bungo_mgmt
+M -rtl -v -u bungo bungo_core
+M -rtl -v -u bungo bungo_top    -i {bungo_core bungo_mgmt}
+M -tb  -v -u bungo tb_bungo_top -i {bungo_top}
+
+# Equivalent -tree in more natural top to bottom order
+#M -unit "bungo" -tree {
+#    (tb) tb_bungo_top
+#    |
+#    \-- (rtl) bungo_top
+#          |
+#          +-- (rtl) bungo_core
+#          |
+#          +-- (rtl) bungo_mgmt
+#}
+
+
+# resources have to be registered first as a module before instances can be created
+M -resource common_sync
+M -tb -v -u test tb_sync -i {common_sync<data> common_sync<valid> common_sync<gpio1,gpio2>}
+
+# Equivalent -tree
+#M -unit "test" -tree {
+#   (tb) tb_sync
+#    |
+#    +--- (res) common_sync<data>
+#    |
+#    +--- (res) common_sync<valid>
+#    |
+#    +--- (res) common_sync<gpio1,gpio2>
+#}
+
 
 ## connections
-S clk              proc_mgmt   -> {tb_proc_top proc_top:clk_test! proc_core:clk!}
-S clk_ref          tb_proc_top -> {proc_mgmt:clk_src!}
-S config -w 32     proc_mgmt   -> proc_core
-S data   -w DATA_W proc_mgmt   -> proc_core
-S status -w 16     proc_mgmt  <-  proc_core
+S clk              bungo_mgmt   -> {tb_bungo_top bungo_top:clk_test! bungo_core:clk!}
+S clk_ref          tb_bungo_top -> {bungo_mgmt:clk_src!}
+S config -w 32     bungo_mgmt   -> bungo_core
+S data   -w DATA_W bungo_mgmt   -> bungo_core
+S status -w 16     bungo_mgmt  <-  bungo_core
 
-S bddata -w 16 -b  {proc_mgmt proc_core tb_proc_top}
+S bddata -w 16 -b  {bungo_mgmt bungo_core tb_bungo_top}
 
 # code
-C proc_mgmt -a {
+C bungo_mgmt -a {
     assign clk = clk_ref;
 }
 
 # parameters
-P DATA_W -v 32 {proc_mgmt proc_core tb_proc_top}
+P DATA_W -v 32 {bungo_mgmt bungo_core tb_bungo_top}
 
 # vim: set filetype=icglueconstructtcl :
