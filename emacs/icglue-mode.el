d31075c51c72ed7d77ee0aff2e5fa39c78749ed2
Heiner Bauer <heiner.bauer@tu-dresden.de>
2019-07-04 10:29:57 +0200

    bump version

--- emacs/icglue-mode.el
+++ emacs/icglue-mode.el
@@ -5,7 +5,7 @@
 
 ;; Keywords: icglue tcl generator sng eda verilog systemverilog vhdl vlsi
 
-;; Version: 0.0.3
+;; Version: 0.0.4
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
