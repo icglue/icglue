982d5fd6180f1ea82d3088cec18874aa865bcdbe
Andreas Dixius <andreas.dixius@icglue.org>
2019-07-13 16:50:06 +0200

    prep-templates: iverilog - allow different src files

--- templates-prep/iverilog/init.tcl
+++ templates-prep/iverilog/init.tcl
@@ -6,6 +6,7 @@ proc template_args {} {
     return {
         "--unit"     "<unit-name>"     {}               {$value ne {}}
         "--testcase" "<testcase-name>" {}               {$value ne {}}
+        "--srcset"   "<src-set-name>"  {rtl}            {$value ne {}}
     }
 }
 
@@ -13,12 +14,13 @@ proc template_args {} {
 proc template_data {userdata tdir} {
     set unit [dict get $userdata "--unit"]
     set tc   [dict get $userdata "--testcase"]
+    set src  [dict get $userdata "--srcset"]
 
     return [subst {
         copy "${tdir}/Makefile.project.iverilog.template" "env/iverilog/Makefile.project.iverilog"
-        icgt "${tdir}/Makefile.rtl.sources.template"      "units/${unit}/simulation/iverilog/common/Makefile.rtl.sources"
+        icgt "${tdir}/Makefile.rtl.sources.template"      "units/${unit}/simulation/iverilog/common/Makefile.${src}.sources"
         copy "${tdir}/Makefile.iverilog.template"         "units/${unit}/simulation/iverilog/common/Makefile.iverilog"
-        link "../common/Makefile.rtl.sources"             "units/${unit}/simulation/iverilog/${tc}/Makefile.rtl.sources"
+        link "../common/Makefile.${src}.sources"          "units/${unit}/simulation/iverilog/${tc}/Makefile.rtl.sources"
         link "../common/Makefile.iverilog"                "units/${unit}/simulation/iverilog/${tc}/Makefile"
     }]
 }
