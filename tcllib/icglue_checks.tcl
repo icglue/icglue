09af73d795b523faad8d88b4aa303167d0c94dd4
Felix Neumärker <felix.neumaerker@icglue.org>
2019-10-11 16:57:35 +0200

    version update to 5.0a1

--- tcllib/icglue_checks.tcl
+++ tcllib/icglue_checks.tcl
@@ -17,7 +17,7 @@
 #   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-package provide ICGlue 4.0
+package provide ICGlue 5.0a1
 
 ## @brief Sanity/consistency checks for objects
 namespace eval ig::checks {
