17459c309bff97159248af70feeed1a2fdc27e5e
Felix Neumärker <felix.neumaerker@icglue.org>
2019-11-22 16:43:07 +0100

    templates-prep: update env.sh for bash completions

--- templates-prep/project/env.sh
+++ templates-prep/project/env.sh
@@ -1,37 +1,73 @@
-src=${BASH_SOURCE:-$_}
+#
+# this is  bash and zsh compatible script,
+# which should be placed in the icpro project root.
+#
+# source this script to setup the icpro project enviroment by providing
+#
+#    - $ICPRO_DIR variable
+#    - cdi, cdu, cds function to change directories
+#    - completions for the upper functions
+#    - prompt shortening
+#
 
+src=${BASH_SOURCE:-$_}
 export ICPRO_DIR=$(dirname $(readlink -e $src))
 unset src
 
-if alias cdi &> /dev/null ; then unalias cdi ; fi
-if alias cdu &> /dev/null ; then unalias cdu ; fi
-if alias cds &> /dev/null ; then unalias cds ; fi
+[[ -n "$(declare -f -F icpro_cd)" ]] || icpro_cd() {
+    local flags
+    local dir
+    if [[ -n "$ICPRO_DIR" ]] ; then
+        dir=$ICPRO_DIR$1
+        shift
+        if [[ $1 = "-P" || $1 == "-L" ]] ; then
+            flags=$1
+            shift
+        fi
 
-function cdi () {
-    if [ "_$ICPRO_DIR" != "_" ] ; then
-        cd "$ICPRO_DIR/$@"
-    else
-        echo '$ICPRO_DIR environment variable not set'
-    fi
-}
-function cdu () {
-    if [ "_$ICPRO_DIR" != "_" ] ; then
-        cd "$ICPRO_DIR/units/$@"
-    else
-        echo '$ICPRO_DIR environment variable not set'
-    fi
-}
-function cds () {
-    if [ "_$ICPRO_DIR" != "_" ] ; then
-        cd "$ICPRO_DIR/software/$@"
+        cd $flags ${dir%/}/$@
     else
         echo '$ICPRO_DIR environment variable not set'
     fi
 }
 
-if echo "$SHELL" | grep zsh &> /dev/null ; then
-    compctl -W $ICPRO_DIR/ -/ cdi
-    compctl -W $ICPRO_DIR/units/ -/ cdu
+[[ -n "$(declare -f -F cdi)" ]] || cdi() {
+    icpro_cd / $@
+}
+[[ -n "$(declare -f -F cdu)" ]] || cdu() {
+    icpro_cd /units $@
+}
+[[ -n "$(declare -f -F cds)" ]] || cds () {
+    icpro_cd /software $@
+}
+
+if [[ -n "$BASH_VERSION" ]]; then
+    for f in cdi cdu cds ; do
+        eval "_bashcomp_$f() { COMPREPLY=(\$($f > /dev/null 2> /dev/null && compgen -A directory -S / -- \$2)) ; }"
+        complete -o nospace -F _bashcomp_$f $f
+    done
+    if [[ -z $PROMPT_COMMAND ]] ; then
+        _icpro_prompt () {
+            PS1=$(echo "${_icpro_prompt_base@P}" | sed -e "s#${ICPRO_DIR/$HOME/\~}#ICPRO_DIR#g")
+        }
+        _icpro_prompt_base="$PS1"
+        export PROMPT_COMMAND=_icpro_prompt
+    fi
+fi
+
+if [[ -n "$ZSH_VERSION" ]]; then
+    : ~ICPRO_DIR
+    compctl -W $ICPRO_DIR/          -/ cdi
+    compctl -W $ICPRO_DIR/units/    -/ cdu
     compctl -W $ICPRO_DIR/software/ -/ cds
 fi
 
+icpro_logout() {
+    if [[ -n "$(declare -f -F _icpro_prompt)" ]] ; then
+        PS1="$_icpro_prompt_base"
+        unset -f _icpro_prompt
+        unset _icpro_prompt_base
+        unset PROMPT_COMMAND
+    fi
+    unset ICPRO_DIR
+}
