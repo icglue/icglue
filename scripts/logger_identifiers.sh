8fcf16a31f536eb1d108e06dc32f2c5f9b867985
Felix Neumärker <felix.neumaerker@icglue.org>
2020-03-03 14:14:18 +0100

    zsh-completion: add for icglue

--- scripts/logger_identifiers.sh
+++ scripts/logger_identifiers.sh
@@ -4,10 +4,16 @@ root_dir=$(readlink -e $(dirname $0)/..)
 
 echo "TCL Log-Identifier:"
 echo "-------------------"
-grep -h ig::log  $root_dir/bin/* $root_dir/tcllib/* | sed -nr -e 's/.*-id\s(\w+).*/\1/p' | sort -u
+grep -h ig::log  $root_dir/bin/* $root_dir/tcllib/* | grep -- '-debug' | sed -nr -e 's/.*-id\s(\w+).*/D,\1/p' | sort -u
+grep -h ig::log  $root_dir/bin/* $root_dir/tcllib/* | grep -- '-info'  | sed -nr -e 's/.*-id\s(\w+).*/I,\1/p' | sort -u
+grep -h ig::log  $root_dir/bin/* $root_dir/tcllib/* | grep -- '-warn'  | sed -nr -e 's/.*-id\s(\w+).*/W,\1/p' | sort -u
+grep -h ig::log  $root_dir/bin/* $root_dir/tcllib/* | grep -- '-error' | sed -nr -e 's/.*-id\s(\w+).*/E,\1/p' | sort -u
 
 echo ""
 echo "C Log-Identifier:"
 echo "-----------------"
-egrep -h '\<log_(debug|info|warn|error)' $root_dir/lib/*.c  | sed -nr -e 's/.*log_(debug|info|warn|error)\s*\("([^"]+)".*/\2/p' | sort -u
+egrep -h '\<log_debug' $root_dir/lib/*.c | sed -nr -e 's/.*log_debug\s*\("([^"]+)".*/D,\1/p' | sort -u
+egrep -h '\<log_info' $root_dir/lib/*.c  | sed -nr -e 's/.*log_info\s*\("([^"]+)".*/I,\1/p'  | sort -u
+egrep -h '\<log_warn' $root_dir/lib/*.c  | sed -nr -e 's/.*log_warn\s*\("([^"]+)".*/W,\1/p'  | sort -u
+egrep -h '\<log_debug' $root_dir/lib/*.c | sed -nr -e 's/.*log_debug\s*\("([^"]+)".*/E,\1/p' | sort -u
 
