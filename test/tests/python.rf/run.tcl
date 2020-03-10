1f918965ce94e4ec457e0de77a65726919357c80
Heiner Bauer <Heiner.Bauer@tu-dresden.de>
2020-03-10 12:16:53 +0100

    add simple test for generated python regfile

---
+++ test/tests/python.rf/run.tcl
@@ -0,0 +1,19 @@
+# test files
+deploy python_rf.icglue           units/python_rf/source/gen/
+
+
+# setup
+run icprep project
+
+# generate everything
+run icglue units/python_rf/source/gen/python_rf.icglue
+
+# check for 0 warnings/errors
+eval_run_output {
+    glob {W,*:*} 0
+    glob {E,*:*} 0
+}
+deploy_resource python-regfile
+deploy tc_python_rf.py software/python/regfile_access/
+
+run python software/python/regfile_access/tc_python_rf.py
---
+++ test/tests/python.rf/tc_python_rf.py
@@ -0,0 +1,16 @@
+from rf_python_rf import rf_python_rf
+
+def read(addr):
+    print ("Reading addr 0x%08X" % (addr))
+    return 0
+
+def write(addr, value):
+    print ("Writing addr 0x%08X = 0x%08X" % (addr, value))
+
+regfile = rf_python_rf(0, read, write)
+p  = regfile.gpio_ctrl._word
+regfile.gpio_ctrl._word = 0
+p = regfile.gpio_ctrl.do
+regfile.gpio_ctrl.do = 4
+d = regfile.gpio_ctrl._dict
+regfile.gpio_ctrl._dict = {'do':1, 'oe':2, 'gpio_mux':3}
