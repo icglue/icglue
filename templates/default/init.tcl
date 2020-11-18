dfb5668bd120e53e5eb5fc68ed51ce712054a23f
Heiner Bauer <Heiner.Bauer@tu-dresden.de>
2020-11-18 13:34:17 +0100

    add template for dummy liberty files

--- templates/default/init.tcl
+++ templates/default/init.tcl
@@ -11,6 +11,8 @@ proc template_data {userdata tdir} {
         set mode   [ig::db::get_attribute -object $object -attribute "mode"       -default "rtl"]
         set lang   [ig::db::get_attribute -object $object -attribute "language"]
 
+        set gen_dummy_liberty [ig::db::get_attribute -object $object -attribute "gen_dummy_liberty" -default false]
+
         foreach {ilang itag itype idir iext lexcom} {
                 verilog       vlog  wtf  verilog       v   {"/* " " */"}
                 systemverilog svlog icgt systemverilog sv  {"/* " " */"}
@@ -21,6 +23,12 @@ proc template_data {userdata tdir} {
             if {$lang eq $ilang} {
                 add "${itag}-${iext}" $itype "${tdir}/${itag}/template.${itype}.${iext}" "units/${parent}/source/${mode}/${idir}/${name}.${iext}" $lexcom
             }
+        }
+
+        if {$gen_dummy_liberty} {
+            # opcondition is mandatory, no default
+            set opcondition [ig::db::get_attribute -object $object -attribute "opcondition"]
+            add "dummy-liberty" wtf "${tdir}/dummy-liberty/template.wtf.lib" "units/${parent}/source/dummy-liberty/${name}_${opcondition}.lib" {"/* " " */"}
         }
     } elseif {$type eq "regfile"} {
         foreach {itag itype lexcom} {
