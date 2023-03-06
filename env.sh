473989307e34c7322fb139d4aa7c65cdf9a987f0
Felix Neumärker <felix.neumaerker@icglue.org>
2023-03-06 13:39:24 +0100

    env.sh: use distribute zsh completions

--- env.sh
+++ env.sh
@@ -10,7 +10,11 @@ export MANPATH="${MANPATH}:${pkgsrcpath}/share/man:${pkgsrcpath}/doc/ICGlue/man"
 alias icg='icglue --nocopyright'
 
 if [[ -n ${ZSH_VERSION} ]] ; then
-    compdef _gnu_generic icglue
+    (( $+functions[_icglue] )) || {
+        fpath+=($pkgsrcpath/share/zsh/site-functions)
+        autoload _icglue
+        compdef _icglue icglue
+    }
 fi
 
 unset src
